f959d2b91aa884c7fd4711dcf10940e07092f2eb
Denis G <denisguriyanov@gmail.com>
2021-11-04 01:58:25 +0300

    added history custom settings

---
+++ user.js-overrides/_base-history.js
@@ -0,0 +1,21 @@
+/* Personal Firefox settings. Based by arkenfox/user.js      */
+/* by Denis G. (https://github.com/denis-g/firefox-settings) */
+
+user_pref("_user.js.status-log", "HISTORY -- error");
+
+
+/******************************************************************************
+ HISTORY
+******************************************************************************/
+
+// custom history settings
+user_pref("privacy.history.custom", true);
+
+// disable search and form history
+user_pref("browser.formfill.enable", false);
+
+// increase the amount of history that is retained
+user_pref("places.history.expiration.max_pages", 10000000);
+
+
+user_pref("_user.js.status-log", "HISTORY -- success");
--- user.js-overrides/_base-sessions.js
+++ user.js-overrides/_base-sessions.js
@@ -8,9 +8,6 @@ user_pref("_user.js.status-log", "SESSIONS -- error");
  SESSIONS
 ******************************************************************************/
 
-// enable search and form history
-user_pref("browser.formfill.enable", true);
-
 // enable session
 user_pref("browser.startup.page", 3);
 
@@ -19,9 +16,6 @@ user_pref("privacy.sanitize.sanitizeOnShutdown", false);
 
 // keep cookies and site data on close
 user_pref("network.cookie.lifetimePolicy", 0);
-
-// increase the amount of history that is retained
-user_pref("places.history.expiration.max_pages", 10000000);
 
 
 user_pref("_user.js.status-log", "SESSIONS -- success");
