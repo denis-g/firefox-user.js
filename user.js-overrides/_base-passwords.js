64029c056333dbcd85100dfb632732cc9416b97e
Denis G <denisguriyanov@gmail.com>
2022-05-18 01:34:58 +0300

    update password settings

--- user.js-overrides/_base-passwords.js
+++ user.js-overrides/_base-passwords.js
@@ -10,7 +10,9 @@ user_pref("_user.js.status-log", "PASSWORDS -- error");
 
 // disable saving passwords
 user_pref("signon.rememberSignons", false);
+user_pref("signon.generation.available", false);
 user_pref("signon.generation.enabled", false);
+user_pref("signon.management.page.enabled", false);
 user_pref("signon.management.page.breach-alerts.enabled", false);
 user_pref("signon.management.page.breachAlertUrl", "");
 
